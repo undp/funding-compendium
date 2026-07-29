5bc67f93bc36a8194ae100f823e922609af82395
nicolebaster-design <105657761+nicolebaster-design@users.noreply.github.com>
2026-07-29 12:15:09 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,59 @@
+# UNDP Design System starter template
+Boilerplate project for sites using [UNDP Design System](https://design.undp.org)
+
+## What's included?
+
+### **Baseline stylesheet** (13.3KB minified and gzipped)
+- [Grid system](https://design.undp.org/?path=/story/foundation-layout-grid--page)
+- UNDP official web typefaces: ProximaNova and Söhne
+- Design System variables available as [CSS custom properties (variables)](https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties)
+- basic HTML elements styling:
+  - [H1-6 headings](https://design.undp.org/?path=/story/foundation-typography-base-typography--headings)
+  - Paragraph text & [links](https://design.undp.org/?path=/story/foundation-typography-links--links)
+  - [Inline elements](https://design.undp.org/?path=/docs/foundation-typography-base-typography) (abbr, blockuote, cite, code, mark, quotation)
+  - [Image](https://design.undp.org/?path=/story/foundation-images-image-with-credit-caption--image-with-credit-caption) and Video default styling, including captions and credit
+  - [Table](https://design.undp.org/?path=/story/foundation-typography-table--table)
+- served via jsDelivr CDN with <20ms latency
+
+### **Ready to use static site generator**
+Inspired by [ZURB Template](https://get.foundation/sites/docs/starter-projects.html#zurb-template)
+- Sass compilaton. Using DartSass library with source maps support.
+- Javascript compilation. Transpiled with [SWC](https://swc.rs/), packaged with [Webpack](https://webpack.js.org/)
+- Assets copying for distribution. Image compressed and optimized using [imagemin](https://github.com/imagemin/imagemin)
+- Page compilation. Based on flat file compiler [Panini](https://get.foundation/sites/docs/panini.html) using [Handlebars](https://handlebarsjs.com/) template language
+- Development mode with [BrowserSync](https://browsersync.io/) server, file watching and on the fly compilation
+- Content decoupled from layout for easy translation via data files
+
+### Dependencies (referenced from external CDN)
+- [jQuery](https://jquery.com/)
+- [GSAP](https://greensock.com/gsap/) with ScrollTrigger and EasePack plugins - animations and transitions
+- [Swiper](https://swiperjs.com/) - carousels and sliders
+
+### **Usage**
+
+Clone project repo locally
+To manually set up the template, first download it with Git:
+```
+git clone https://github.com/undp/design-system-starter-template projectname
+cd projectname
+```
+
+Install dependencies
+```
+npm install
+```
+
+Run development server (at http://localhost:8000)
+```
+npm run start
+```
+
+When ready for publication build production ready assets (published to /docs directory)
+```
+npm run build
+```
+
+Don't forget to relink Git to your own repository:
+```
+git remote set-url origin <remote_url>
+```
---
+++ gulpfile.mjs
@@ -0,0 +1,210 @@
+// inspired by Zurb Foundation starter project
+// https://github.com/foundation/foundation-zurb-template
+
+import browser from 'browser-sync';
+import gulp from 'gulp';
+import panini from 'panini';
+import { rimraf } from 'rimraf';
+import webpackStream from 'webpack-stream';
+import webpack2 from 'webpack';
+import named from 'vinyl-named';
+import imagemin from 'gulp-imagemin';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import autoprefixer from 'autoprefixer';
+import postcss from '@sequencemedia/gulp-postcss';
+import cssnano from 'cssnano';
+import defaultPreset from 'cssnano-preset-default';
+import sourcemaps from 'gulp-sourcemaps';
+import iif from 'gulp-if';
+import TerserPlugin from 'terser-webpack-plugin';
+
+const sass = gulpSass(dartSass);
+
+// file locations
+const PATH_DIST = 'dist';
+const PATH_PUBLISH = 'docs';
+const PATH_ASSETS = ['src/assets/**/*', '!src/assets/{img,js,scss}/**/*'];
+const PATH_SASS = ['node_modules/@undp/design-system/stories'];
+const PATH_JS = 'src/assets/js/app.js';
+const PORT = 8000;
+
+// Check for --production flag
+const PRODUCTION = process.argv.includes('--production');
+
+// Build the "docs" folder by running all of the below tasks
+// Sass must be run later so UnCSS can search for used classes in the others assets.
+gulp.task('build',
+  gulp.series(clean, gulp.parallel(pages, javascript, images, copy), sassBuild, publish)
+);
+
+// Build the site, run the server, and watch for file changes
+gulp.task('default',
+  gulp.series('build', server, watch)
+);
+
+// Delete the "docs" folder
+// This happens every time a build starts
+function clean(done) {
+  rimraf.sync(PATH_DIST);
+  done()
+}
+
+// Copy files out of the assets folder
+// This task skips over the "img", "js", and "scss" folders, which are parsed separately
+function copy() {
+  return gulp.src(PATH_ASSETS, { encoding: false })
+    .pipe(gulp.dest(PATH_DIST + '/assets'));
+}
+
+// copy compiled assets to final destination
+function publish() {
+  return gulp.src(PATH_DIST + '/**/*', { encoding: false })
+    .pipe(iif(PRODUCTION, gulp.dest(PATH_PUBLISH)));
+}
+
+// Copy page templates into finished HTML files
+function pages() {
+  return gulp.src('src/pages/**/*.{html,hbs,handlebars}')
+    .pipe(panini({
+      root: 'src/pages/',
+      layouts: 'src/layouts/',
+      partials: 'src/partials/',
+      data: 'src/data/',
+      helpers: 'src/helpers/'
+    }))
+    .pipe(gulp.dest(PATH_DIST));
+}
+
+// Load updated HTML templates and partials into Panini
+function resetPages(done) {
+  panini.refresh();
+  done();
+}
+
+// Compile Sass into CSS
+// In production, the CSS is compressed
+const preset = defaultPreset();
+
+function sassBuild() {
+
+  return gulp.src('src/assets/scss/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync({
+      includePaths: PATH_SASS
+    }).on('error', sass.logError))
+    .pipe(iif(PRODUCTION, postcss([cssnano({ preset, plugins: [autoprefixer] })])))
+    .pipe(iif(!PRODUCTION, sourcemaps.write()))
+    .pipe(gulp.dest(PATH_DIST + '/assets/css'))
+    .pipe(browser.reload({ stream: true }));
+}
+
+let webpackConfig = {
+  mode: (PRODUCTION ? 'production' : 'development'),
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        minify: TerserPlugin.swcMinify
+      })
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'swc-loader',
+          options: {
+            module: {
+              type: "es6"
+            },
+            minify: process.env.NODE_ENV !== "development",
+            isModule: true,
+            jsc: {
+              minify: {
+                compress: true,
+                mangle: true,
+                format: {
+                  asciiOnly: true,
+                  comments: /^ webpack/
+                }
+              },
+              target: "es2016",
+              parser: {
+                syntax: "typescript",
+                tsx: true
+              },
+              transform: {
+                react: {
+                  runtime: "automatic"
+                }
+              }
+            }
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    jquery: 'jQuery',
+    gsap: 'gsap',
+    swiper: 'swiper',
+  },
+  devtool: !PRODUCTION && 'source-map'
+}
+
+// Combine JavaScript into one file
+// In production, the file is minified
+function javascript() {
+  return gulp.src(PATH_JS)
+    .pipe(named())
+    .pipe(sourcemaps.init())
+    .pipe(webpackStream(webpackConfig, webpack2))
+    .pipe(iif(!PRODUCTION, sourcemaps.write()))
+    .pipe(gulp.dest(PATH_DIST + '/assets/js'));
+}
+
+// Copy images to the "dist" folder
+// In production, the images are compressed
+function images() {
+  return gulp.src('src/assets/img/**/*', { encoding: false })
+    // .pipe(iif(PRODUCTION, imagemin([
+    //   imagemin.gifsicle({ interlaced: true }),
+    //   imagemin.mozjpeg({ quality: 85, progressive: true }),
+    //   imagemin.optipng({ optimizationLevel: 5 }),
+    //   imagemin.svgo({
+    //     plugins: [
+    //       { removeViewBox: true },
+    //       { cleanupIDs: false }
+    //     ]
+    //   })
+    // ])))
+    .pipe(gulp.dest(PATH_DIST + '/assets/img'));
+}
+
+// Start a server with BrowserSync to preview the site in
+function server(done) {
+  browser.init({
+    server: PATH_DIST,
+    port: PORT,
+  }, done);
+}
+
+// Reload the browser with BrowserSync
+function reload(done) {
+  browser.reload();
+  done();
+}
+
+// Watch for changes to static assets, pages, Sass, and JavaScript
+function watch() {
+  gulp.watch(PATH_ASSETS, copy);
+  gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
+  gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
+  gulp.watch('src/data/**/*.{js,json,yml}').on('all', gulp.series(resetPages, pages, browser.reload));
+  gulp.watch('src/helpers/**/*.js').on('all', gulp.series(resetPages, pages, browser.reload));
+  gulp.watch('src/assets/scss/**/*.scss').on('all', sassBuild);
+  gulp.watch('src/assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
+  gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
+}
